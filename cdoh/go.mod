7c4c8d1614b76fcd146a8907ef45a9a5b2a2c128
Ishwor Gurung <me@ishworgurung.com>
2021-01-03 17:19:07 +1100

    add cdoh - cmdline DoH client (A and AAAA for now)

---
+++ cdoh/go.mod
@@ -0,0 +1,5 @@
+module github.com/ishworgurung/go-experiments/cdoh
+
+go 1.15
+
+require github.com/babolivier/go-doh-client v0.0.0-20201028162107-a76cff4cb8b6
---
+++ cdoh/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	"github.com/babolivier/go-doh-client"
+)
+
+func main() {
+	fqdnAddr := flag.String("fqdn", "", "-fqdn example.net")
+	resolverAddr := flag.String("resolver", "", "-resolver 8.8.8.8")
+	flag.Parse()
+	if len(*fqdnAddr) == 0 || len(*resolverAddr) == 0 {
+		flag.Usage()
+		os.Exit(-1)
+	}
+	resolver := doh.Resolver{
+		Host:  *resolverAddr,
+		Class: doh.IN,
+	}
+	a4, ttl4, err := resolver.LookupA(*fqdnAddr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s. %d IN A %s\n", *fqdnAddr, ttl4, a4[0].IP4)
+
+	a6, ttl6, err := resolver.LookupAAAA(*fqdnAddr)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s. %d IN A %s\n", *fqdnAddr, ttl6, a6[0].IP6)
+}
